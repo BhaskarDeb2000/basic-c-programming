ff3f07c988c8676d573c2b0ad2917358bc9fb8d3
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-04-18 12:30:30 +0300

    docs: basic c initialize

---
+++ helloWorld.c
@@ -0,0 +1,31 @@
+/* #include - Calling out a library
+stdio - standard input output
+int main - integer main function
+printf("") - output
+return 0 - to run the program onces
+*/
+
+#include <stdio.h>
+// int main()
+// {
+//     printf("Hello World!\n");
+//     printf("I really like Pizza!");
+//     return 0;
+// }
+
+void functionName()
+{
+    printf("Hello DebArghya!\n");
+    printf("I really like Pizza!");
+    // return 0;
+}
+
+int main()
+{
+
+    functionName();
+}
+
+int play()
+{
+}
