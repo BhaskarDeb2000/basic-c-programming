8fbc70e3fb5d065882ba332028d6117c6d57cf2b
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-04-22 14:41:00 +0300

    doc:for loop in c

---
+++ forLoop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int i;
+    for (int i = 1; i <= 1; i++)
+    {
+        scanf("%d", &i);
+        if (i == 1)
+        {
+            printf("one\n");
+        }
+        else
+        {
+            printf("No one Found\n");
+        }
+    }
+    return 0;
+}
